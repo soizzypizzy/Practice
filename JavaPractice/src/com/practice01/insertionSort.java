6f2c5f03220a2b82c853581b1c24439554b6a627
soizzypizzy <73834311+soizzypizzy@users.noreply.github.com>
2021-08-14 00:27:43 +0900

    삽입정렬

--- JavaPractice/src/com/practice01/insertionSort.java
+++ JavaPractice/src/com/practice01/insertionSort.java
@@ -18,7 +18,24 @@ public class insertionSort {
 		}
 		print(arr);
 		
+		// 오름차순
 		insertionAsc(arr, n);
+		print(arr);
+		
+		// 내림차순
+		insertionDesc(arr, n);
+		print(arr);
+	}
+
+	private static void insertionDesc(int[] a, int n) {
+		for(int i = 1; i < n; i++) {
+			int j;
+			int tmp = a[i];
+			for(j = i; j > 0 && a[j-1] < tmp; j--) {
+				a[j] = a[j-1];
+			}
+			a[j] = tmp;
+		}
 	}
 
 	private static void insertionAsc(int[] a, int n) {
@@ -28,13 +45,13 @@ public class insertionSort {
 			for(j = i; j > 0 && a[j-1] > tmp; j--) {
 				a[j] = a[j-1];
 			}
-			a[j-1] = tmp;
+			a[j] = tmp;
 		}
 	}
 
-	private static void print(int[] arr) {
-		for(int i = 0; i < arr.length; i++) {
-			System.out.print(arr[i] + " ");
+	private static void print(int[] a) {
+		for(int i = 0; i < a.length; i++) {
+			System.out.print(a[i] + " ");
 		}
 		System.out.println();
 	}
